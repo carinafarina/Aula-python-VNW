029c3d8e4017d937dd4e270ed93176e1eded0940
Carina Farina <carinafarina2014@gmail.com>
2025-02-03 19:39:50 -0300

    Subindo mais aula do Python

--- main.py
+++ main.py
@@ -1,5 +1,131 @@
-print("Olá mundo!!!")
-print("Primeira aula!!!")
+# print("Olá mundo!!!")
+# print("Primeira aula!!!")
 
-nome = 'Carina'
-print(nome)
+#nome = 'Carina'
+# print(nome)
+
+#nome_sobrenome = "Farina"
+# print(nome,nome_sobrenome)
+
+# tipos de dados 
+# interiro (int)
+
+#idade = 18
+#frio = -10
+
+# float
+
+#altura = 1.75 
+#graus = -2.2
+
+#string (str)
+
+#frase = "Boa noite !!"
+
+# boolean
+
+#tem_saldo = True
+
+# Null
+
+#idade = None
+
+# not defined
+
+#  dia 
+#  print(dia)
+
+# Exibir soma 
+
+
+# primeiro_valor = 2.2
+# segundo_valor = 2.1
+# print( primeiro_valor + segundo_valor)
+
+#Exibir frasse
+
+# nome = "Marina"
+# frase = "Chuvinha de like"
+# print(nome, frase)
+
+
+# idade = 40
+# nome = "Carina"
+
+# print(type(idade))
+# print(type(nome))
+
+
+###----------------------------------------------------------
+
+# nome = input("Qual seu nome: ")
+# print("olá", nome)
+
+#------------------------------------------------------------
+
+# primeiro_valor = int(input("Digite o primeiro numero: "))
+# segundo_valor = int(input("Digite o segundo numero: "))
+
+# soma = primeiro_valor + segundo_valor
+
+# print("A soma dos valores são: ", soma )
+
+#------------------------Condições---------------------------
+
+#saldo = True
+
+#if saldo == True:
+    #print('Você está com dinheiro!')
+
+#---------------------------------------------
+
+#idade = 40
+
+#if idade >= 18:
+    #print("Você pode dirigir!")
+#else:
+    #print("Você nâo tem idade para dirigir")
+
+#---------------------------------------------
+
+#idade = 18
+#saldo = True        
+
+#if idade >= 18 and saldo  == True: 
+    #print('Você pode tirar sua habilitação')
+#elif idade <18 and saldo == False:   
+    #print('Você não tem idade e dinheiro para tirar habilitação') 
+#else:
+    #print('Você pode dirigir no vai na web')    
+#------------------Operadores logicos--------
+
+# and # == &&
+# or # == ||
+# not # == !
+
+# for é uma estrutura de repetição (loop)
+
+# for caracter in 'Python':
+    #print(caracter)
+
+# lista_dos_sonhos = ["Iate", "Ferrari", "Resort", "Jatinho"]
+
+# for item in lista_dos_sonhos:
+    #print(item)
+
+#-----------------
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    contador += 1
+#------------------
+lanche = 'Pizza'
+print(lanche.upper())
+#------------------
+lanche = 'Pizza'
+print(lanche.lower())
+#---tamplete string----
+lanche = 'Pizza'
+print(f'seu lanche é:{lanche}')
+
